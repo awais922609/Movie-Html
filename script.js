edf473200844e6c550c16089f2b6d0c2f64d3343
awais922609 <awaissajid92260@gmail.com>
2023-07-13 12:47:05 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d56b4fa876173ab71e8ab9a53f21d429&page=1';
+const IMG_PATH='https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=d56b4fa876173ab71e8ab9a53f21d429&query=";
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+
+returnMovies(APILINK);
+
+function returnMovies(url) {
+  fetch(url)
+    .then(res => res.json())
+    .then(function(data) {
+      data.results.forEach(Element => {
+        const div_card = document.createElement('div');
+        div_card.setAttribute('class', 'card');
+        
+        const div_row = document.createElement('div');
+        div_row.setAttribute('class', 'row');
+        
+        const div_column = document.createElement('div');
+        div_column.setAttribute('class', 'column');
+        
+        const image = document.createElement('img');
+        image.setAttribute('class', 'thumbnail');
+        image.setAttribute('id', 'image');
+        image.src = IMG_PATH + Element.poster_path;
+        
+        const title = document.createElement('div');
+        title.setAttribute('id', 'title');
+        title.innerHTML = `${Element.title}`;
+        
+        const center = document.createElement('div');
+        center.appendChild(image);
+        div_card.appendChild(center);
+        div_card.appendChild(title);
+        div_column.appendChild(div_card);
+        div_row.appendChild(div_column);
+        main.appendChild(div_row);
+      });
+    });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  main.innerHTML = "";
+
+  const searchItem = search.value;
+  returnMovies(SEARCHAPI + searchItem);
+  search.value = "";
+});
